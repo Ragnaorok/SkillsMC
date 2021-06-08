fc3ce9825b4fbfb2aa34c2a8a51be4497a5879dd
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-08 16:21:22 -0700

    Removed Enchantment Table Ring

--- src/main/java/ragnaorok/Main/LevelUpListener.java
+++ src/main/java/ragnaorok/Main/LevelUpListener.java
@@ -16,12 +16,11 @@ public class LevelUpListener implements Listener {
         Location particleLoc = ploc.clone();
         int pre_Level = (player.getLevel() - 1);
         if (player.getLevel() > pre_Level) {
-            player.spawnParticle(Particle.FIREWORKS_SPARK, ploc, 20);
             for (int i = 0; i < 360; i += 5) {
-                particleLoc.setY(ploc.getY()+1);
-                particleLoc.setZ(ploc.getZ() + Math.sin(i) * 3);
-                particleLoc.setX(ploc.getX() + Math.cos(i) * 3);
-                player.spawnParticle(Particle.ENCHANTMENT_TABLE, particleLoc, 50);
+                particleLoc.setY(ploc.getY()+2);
+                particleLoc.setZ(ploc.getZ() + Math.sin(i) * 1);
+                particleLoc.setX(ploc.getX() + Math.cos(i) * 1);
+                player.spawnParticle(Particle.END_ROD, particleLoc, 1);
             }
         }
     }
