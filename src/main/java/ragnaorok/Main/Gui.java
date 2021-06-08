82a3e82722abde5e1d3ffed672843adbc98d507a
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-08 16:17:22 -0700

    Prototype Gui disabled

--- src/main/java/ragnaorok/Main/Gui.java
+++ src/main/java/ragnaorok/Main/Gui.java
@@ -4,12 +4,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
-import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -19,15 +17,29 @@ public class Gui implements Listener {
     private Inventory gui;
 
     public void openNewGui(Player player) {
-        gui = Bukkit.createInventory(null, InventoryType.HOPPER);
+        gui = Bukkit.createInventory(null,9,ChatColor.AQUA + "Slayer Skills");
 
-        ItemStack item = new ItemStack(Material.GREEN_CONCRETE, 1);
-        ItemMeta meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.GREEN + "Press This");
-        item.setItemMeta(meta);
+        ItemStack blood_Lust_Skill = new ItemStack(Material.END_CRYSTAL);
+        ItemMeta blood_Lust = blood_Lust_Skill.getItemMeta();
+        blood_Lust.setDisplayName(ChatColor.DARK_RED + "Slayer Skill: Blood Lust");
+        blood_Lust_Skill.setItemMeta(blood_Lust);
+        gui.setItem(2, blood_Lust_Skill);
 
-        gui.setItem(2, item);
+        ItemStack second_Wind_Skill = new ItemStack(Material.END_CRYSTAL);
+        ItemMeta second_Wind = second_Wind_Skill.getItemMeta();
+        second_Wind.setDisplayName(ChatColor.GREEN + "Slayer Skill: Second Wind");
+        second_Wind_Skill.setItemMeta(second_Wind);
+        gui.setItem(3, second_Wind_Skill);
+
         player.openInventory(gui);
+    }
+
+    public ItemStack giveItem(String name){
+        ItemStack blood_Lust_Skill = new ItemStack(Material.END_CRYSTAL);
+        ItemMeta blood_Lust = blood_Lust_Skill.getItemMeta();
+        blood_Lust.setDisplayName(ChatColor.DARK_RED + "Slayer Skill: Blood Lust");
+        blood_Lust_Skill.setItemMeta(blood_Lust);
+        return blood_Lust_Skill;
     }
 
     @EventHandler
@@ -35,14 +47,14 @@ public class Gui implements Listener {
         if (!event.getInventory().equals(gui)) {
             return;
         }
-        event.setCancelled(true);
 
+        event.setCancelled(true);
         Player player = (Player) event.getWhoClicked();
 
         switch (event.getSlot()) {
             case 2: {
                 player.closeInventory();
-                player.sendMessage(ChatColor.GREEN + "You pressed it");
+                player.sendMessage(ChatColor.GREEN + "Slayer Skill Selected");
                 break;
             }
         }
@@ -50,8 +62,8 @@ public class Gui implements Listener {
 
     @EventHandler
     public void openGuiEvent(PlayerDropItemEvent event) {
-       Item item =  event.getItemDrop();
-           openNewGui(event.getPlayer());
+        Item item = event.getItemDrop();
+        openNewGui(event.getPlayer());
 
     }
 }
