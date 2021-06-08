e62b23862a19982a6137517ed1bd121246979a5d
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-08 16:21:50 -0700

    Updated Main.java
    removed Gui from main and implemented the skill select class

--- src/main/java/ragnaorok/Main/Main.java
+++ src/main/java/ragnaorok/Main/Main.java
@@ -1,15 +1,19 @@
 package ragnaorok.Main;
 
-import org.bukkit.Particle;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new Gui(), this);
+        PluginManager pm = this.getServer().getPluginManager();
+        pm.registerEvents(new SkillSelectCommand(this), this);
+        this.getCommand("skill").setExecutor(new SkillSelectCommand(this));
         getServer().getPluginManager().registerEvents(new MobListener(), this);
         getServer().getPluginManager().registerEvents(new LevelUpListener(), this);
+        getServer().getPluginManager().registerEvents(new DoubleJump(), this);
+
     }
     @Override
     public void onDisable() {
