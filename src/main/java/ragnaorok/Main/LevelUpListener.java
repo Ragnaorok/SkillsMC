83b01fc62e63e67defd599c0a039f40065735ce6
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-14 14:28:30 -0700

    removed unnecessary *1 in algorithm

--- src/main/java/ragnaorok/Main/LevelUpListener.java
+++ src/main/java/ragnaorok/Main/LevelUpListener.java
@@ -5,8 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
 
 public class LevelUpListener implements Listener {
     @EventHandler
@@ -18,8 +16,8 @@ public class LevelUpListener implements Listener {
         if (player.getLevel() > pre_Level) {
             for (int i = 0; i < 360; i += 5) {
                 particleLoc.setY(ploc.getY()+2);
-                particleLoc.setZ(ploc.getZ() + Math.sin(i) * 1);
-                particleLoc.setX(ploc.getX() + Math.cos(i) * 1);
+                particleLoc.setZ(ploc.getZ() + Math.sin(i));
+                particleLoc.setX(ploc.getX() + Math.cos(i));
                 player.spawnParticle(Particle.END_ROD, particleLoc, 1);
             }
         }
