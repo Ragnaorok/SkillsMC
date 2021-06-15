235dc0e5d63a0fb5d92b6341ca69467e1034518a
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-14 19:58:18 -0700

    New Souls Class
    stores players' souls

---
+++ src/main/java/ragnaorok/Main/Souls.java
@@ -0,0 +1,9 @@
+package ragnaorok.Main;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Souls {
+    public static final HashMap<UUID, Integer> SOULS = new HashMap<UUID, Integer>();
+
+}
