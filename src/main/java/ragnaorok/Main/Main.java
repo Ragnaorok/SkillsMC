5e5e1791f43b5eb82d1f8b8df1be07207235122b
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-14 20:00:20 -0700

    Updated Main
    main updated

--- src/main/java/ragnaorok/Main/Main.java
+++ src/main/java/ragnaorok/Main/Main.java
@@ -2,53 +2,43 @@ package ragnaorok.Main;
 
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import ragnaorok.Main.commands.SkillSelectCommand;
+import ragnaorok.Main.commands.SaveAllCommand;
 import ragnaorok.Main.commands.SoulsCommand;
+import ragnaorok.Main.listeners.BountyListener;
+import ragnaorok.Main.listeners.CrouchJumpListener;
+import ragnaorok.Main.listeners.LevelUpListener;
+import ragnaorok.Main.listeners.MobListener;
 import ragnaorok.Main.managers.BountyManager;
 import ragnaorok.Main.managers.CurrencyManager;
-
-import java.io.IOException;
 
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        CurrencyManager currencyManager = new CurrencyManager(this);
         try {
-            currencyManager.loadCurrencyFile();
-        } catch (Exception e){
+            CurrencyManager.loadCurrencyFile();
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        BountyManager bountyManager = new BountyManager(this);
         try {
-            bountyManager.loadCurrencyFile();
-        } catch (ClassNotFoundException | IOException e){
+            BountyManager.loadCurrencyFile();
+        } catch (Exception e) {
             e.printStackTrace();
         }
         PluginManager pm = this.getServer().getPluginManager();
         //pm.registerEvents(new SkillSelectCommand(this), this);
         //this.getCommand("skill").setExecutor(new SkillSelectCommand(this));
         this.getCommand("souls").setExecutor(new SoulsCommand(this));
+        this.getCommand("saveAll").setExecutor(new SaveAllCommand(this));
         getServer().getPluginManager().registerEvents(new MobListener(), this);
         getServer().getPluginManager().registerEvents(new LevelUpListener(), this);
-        getServer().getPluginManager().registerEvents(new CrouchJump(), this);
+        getServer().getPluginManager().registerEvents(new CrouchJumpListener(), this);
         getServer().getPluginManager().registerEvents(new BountyListener(), this);
-        registerManagers();
-
+        System.out.println("Plugin Enabled");
     }
+
     @Override
     public void onDisable() {
-        CurrencyManager currencyManager = new CurrencyManager(this);
-        try {
-            currencyManager.saveCurrencyFile();
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-    }
-    public void registerManagers(){
-        new BountyManager(this);
-        new CurrencyManager(this);
     }
 }
 
