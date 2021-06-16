1267eaba816bf11ad49a80613c7a7d671e07b190
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-16 16:42:37 -0700

    Added Crit Particles

--- src/main/java/ragnaorok/Main/listeners/CrouchJumpListener.java
+++ src/main/java/ragnaorok/Main/listeners/CrouchJumpListener.java
@@ -1,5 +1,8 @@
 package ragnaorok.Main.listeners;
 
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,7 +14,16 @@ public class CrouchJumpListener implements Listener {
     @EventHandler
     public void onPlayerToggleSneakEvent(PlayerToggleSneakEvent event) {
         Player player = event.getPlayer();
+        World world = player.getWorld();
+        Location loc = player.getLocation();
+        Location particleLoc = loc.clone();
+        Location ploc = loc.clone();
         player.addPotionEffect((new PotionEffect(PotionEffectType.JUMP, 10, 1)));
-
+        for (int i = 0; i < 360; i += 5) { //Magic Circle
+            particleLoc.setY(ploc.getY());
+            particleLoc.setZ(ploc.getZ() + Math.sin(i));
+            particleLoc.setX(ploc.getX() + Math.cos(i));
+            world.spawnParticle(Particle.CRIT_MAGIC, particleLoc, 1);
+        }
     }
 }
