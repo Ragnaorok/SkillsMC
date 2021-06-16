7bcdabe377596e3593966ca94c5392251da79c4c
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-16 16:43:42 -0700

    Removed Stubs, added ChatColor

--- src/main/java/ragnaorok/Main/listeners/BountyListener.java
+++ src/main/java/ragnaorok/Main/listeners/BountyListener.java
@@ -1,17 +1,16 @@
 package ragnaorok.Main.listeners;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import ragnaorok.Main.Main;
 import ragnaorok.Main.managers.BountyManager;
 import ragnaorok.Main.managers.CurrencyManager;
 
 
 public class BountyListener implements Listener {
-    public Main plugin;
     @EventHandler
     public void bountyListener(PlayerDeathEvent event) {
         if (event.getEntity() instanceof Player) {
@@ -19,13 +18,13 @@ public class BountyListener implements Listener {
             if (victim.getKiller() instanceof Player) {
                 Player killer = victim.getKiller();
                 if (BountyManager.getPlayerBounty(victim) == 0) {
-                    BountyManager.addBountyToPlayer(killer, +1);
-                    Bukkit.broadcastMessage(killer.getName() + "'s bounty has increased");
+                    BountyManager.addBountyToPlayer(killer, 1);
+                    Bukkit.broadcastMessage(ChatColor.RED + killer.getName() + "'s bounty has increased");
                 } else if (BountyManager.getPlayerBounty(victim) > 0) {
                     CurrencyManager.removePlayerCurrency(victim, BountyManager.getPlayerBounty(victim));
                     CurrencyManager.addCurrencyToPlayer(killer, BountyManager.getPlayerBounty(victim));
                     BountyManager.setPlayerBounty(victim, 0);
-                    Bukkit.broadcastMessage(killer.getName() + " has killed " + victim.getName() + " and obtained their bounty");
+                    Bukkit.broadcastMessage(ChatColor.GREEN + killer.getName() + " has killed " + victim.getName() + " and obtained their bounty");
                 }
             }
         }
