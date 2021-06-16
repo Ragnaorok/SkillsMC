2cdbba70d2806403ae8a1160570f87c0281249b3
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-16 16:43:59 -0700

    Updated Main.Java

--- src/main/java/ragnaorok/Main/Main.java
+++ src/main/java/ragnaorok/Main/Main.java
@@ -2,12 +2,10 @@ package ragnaorok.Main;
 
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import ragnaorok.Main.commands.KaboomCommand;
 import ragnaorok.Main.commands.SaveAllCommand;
 import ragnaorok.Main.commands.SoulsCommand;
-import ragnaorok.Main.listeners.BountyListener;
-import ragnaorok.Main.listeners.CrouchJumpListener;
-import ragnaorok.Main.listeners.LevelUpListener;
-import ragnaorok.Main.listeners.MobListener;
+import ragnaorok.Main.listeners.*;
 import ragnaorok.Main.managers.BountyManager;
 import ragnaorok.Main.managers.CurrencyManager;
 
@@ -29,11 +27,13 @@ public final class Main extends JavaPlugin {
         //pm.registerEvents(new SkillSelectCommand(this), this);
         //this.getCommand("skill").setExecutor(new SkillSelectCommand(this));
         this.getCommand("souls").setExecutor(new SoulsCommand(this));
+        this.getCommand("kaboom").setExecutor(new KaboomCommand(this));
         this.getCommand("saveAll").setExecutor(new SaveAllCommand(this));
         getServer().getPluginManager().registerEvents(new MobListener(), this);
         getServer().getPluginManager().registerEvents(new LevelUpListener(), this);
         getServer().getPluginManager().registerEvents(new CrouchJumpListener(), this);
         getServer().getPluginManager().registerEvents(new BountyListener(), this);
+        getServer().getPluginManager().registerEvents(new BlockingListener(), this);
         System.out.println("Plugin Enabled");
     }
 
