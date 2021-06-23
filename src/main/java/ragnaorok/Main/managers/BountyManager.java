2edd3b1ce36bde5a245cc8e92e5fe3ef213d35d5
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-22 22:12:17 -0700

    fixed ctrl c + ctrl v mistakes

--- src/main/java/ragnaorok/Main/managers/BountyManager.java
+++ src/main/java/ragnaorok/Main/managers/BountyManager.java
@@ -1,19 +1,16 @@
 package ragnaorok.Main.managers;
 
-import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import ragnaorok.Main.Currency;
-
 import java.io.*;
-import java.nio.file.FileSystemNotFoundException;
-import java.util.UUID;
+import java.util.HashMap;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 public class BountyManager {
 
     public static void saveBountyFile() throws Exception {
-        File file = new File("currency.dat");
+        File file = new File("bounty.dat");
         boolean successful = true;
         if (!file.exists()) {
             successful = file.createNewFile();
@@ -23,7 +20,7 @@ public class BountyManager {
             return;
 
         try (ObjectOutputStream output = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(file)))) {
-            output.writeObject(Currency.SOULS);
+            output.writeObject(Currency.BOUNTIES);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
@@ -42,14 +39,16 @@ public class BountyManager {
         if (!successful)
             return;
 
-        ObjectInputStream input = new ObjectInputStream(new GZIPInputStream(new FileInputStream(file)));
-        try {
-            Object readObject = input.readObject();
-            System.out.println(readObject.toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            input.close();
+        try (ObjectInputStream input = new ObjectInputStream(new GZIPInputStream(new FileInputStream(file)))) {
+            HashMap<String, Integer> readObject = (HashMap<String, Integer>) input.readObject();
+            // deepCopy this object because we don't want to keep a reference to this readObject
+            // which we assume is a HashMap<String, Integer>
+            for (String key : readObject.keySet()) {
+                int val = readObject.get(key);
+                Currency.BOUNTIES.put(key, val);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
