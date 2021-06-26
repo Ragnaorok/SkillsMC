0e7cf9abddfc823a17cfde89af705950ddbd9044
Ragnaorok <Tung02nguyen10@gmail.com>
2021-06-25 23:43:54 -0700

    Updated Netherite Hoe to glide

--- src/main/java/ragnaorok/Main/listeners/ToolListeners/NetheriteHoeListener.java
+++ src/main/java/ragnaorok/Main/listeners/ToolListeners/NetheriteHoeListener.java
@@ -1,15 +1,15 @@
 package ragnaorok.Main.listeners.ToolListeners;
 
 import org.bukkit.*;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import org.bukkit.block.Block;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
 
 import java.util.HashMap;
@@ -19,12 +19,28 @@ public class NetheriteHoeListener implements Listener {
     Map<String, Long> leftCooldown = new HashMap<String, Long>();
     Map<String, Long> shiftCooldown = new HashMap<String, Long>();
 
+    private void groundUpdate(Player player) {  //Cancel glide if player is touching the ground
+        Location location = player.getPlayer().getLocation();
+        location = location.subtract(0, 1, 0);
+
+        Block block = location.getBlock();
+
+        if (!block.getType().isSolid()) return;
+
+        player.setGliding(false);
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onPlayerMove(PlayerMoveEvent event) { //Calls groundUpdate
+        if (!event.getPlayer().isGliding()) return;
+        this.groundUpdate(event.getPlayer());
+    }
+
     @EventHandler
-    public void onBlock(PlayerInteractEvent event) {
+    public void netheriteHoeEvent(PlayerInteractEvent event) { //Main Function for the Netherite Hoe
         Player player = event.getPlayer();  //player
-        Location origin = player.getEyeLocation();  //player origin
+        Location origin = player.getEyeLocation();  //player eye origin
         Vector direction = origin.getDirection();
-        Location oloc = origin.add(direction);  //cloning to prevent weird side effects
         Location destination = origin.clone().add(direction).add(direction); //destination
         World world = player.getWorld();
         EntityType entityType = EntityType.SHULKER_BULLET;
@@ -36,18 +52,15 @@ public class NetheriteHoeListener implements Listener {
                     if (leftCooldown.containsKey(player.getName())) {
                         if (leftCooldown.get(player.getName()) > System.currentTimeMillis()) {
                             long time = (leftCooldown.get(player.getName()) - System.currentTimeMillis()) / 1000;
-                            player.sendMessage(ChatColor.DARK_GRAY + "Reap will be ready in " + time + " second(s)");
+                            player.sendMessage(ChatColor.DARK_GRAY + "Phantom Rush will be ready in " + time + " second(s)");
                             return;
                         }
                     }
-                    leftCooldown.put(player.getName(), System.currentTimeMillis() + (3 * 1000));
+                    leftCooldown.put(player.getName(), System.currentTimeMillis() + (5 * 1000));
                     player.sendMessage(ChatColor.GREEN + "Scythe Skill: Phantom Rush");
                     direction.normalize();
-                    direction.multiply(2);
                     player.setVelocity(direction);
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 30, 3));
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 15, 3));
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 50, 3));
+                    player.setGliding(true);
                 }
                 if (player.isSneaking()) {
                     if (shiftCooldown.containsKey(player.getName())) {
@@ -57,14 +70,23 @@ public class NetheriteHoeListener implements Listener {
                             return;
                         }
                     }
-                    shiftCooldown.put(player.getName(), System.currentTimeMillis() + (6 * 1000));
-                    player.sendMessage(ChatColor.GREEN + "Scythe Skill: Cursed Bullet");
+                    shiftCooldown.put(player.getName(), System.currentTimeMillis() + (7 * 1000));
+                    player.sendMessage(ChatColor.GREEN + "Scythe Skill: Shulker Bullet");
                     destination.add(direction).add(direction);
-                    Entity entity = world.spawnEntity(new Location(world, destination.getX(), destination.getY()-1, destination.getZ()), entityType);
-                    oloc.getWorld().spawnParticle(Particle.PORTAL, oloc, 10);
-                    direction.add(direction);
-                    entity.setVelocity(direction);
+                    Entity entity = world.spawnEntity(new Location(world, destination.getX(), destination.getY() - 1, destination.getZ()), entityType);
+                    Vector vec = player.getLocation().getDirection();
+                    entity.setVelocity(vec.multiply(4));
                 }
+            }
+        }
+    }
+
+    @EventHandler
+    public void reinforceEnchant(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player && event.getEntity() instanceof Monster || event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
+            Player player = (Player) event.getDamager();
+            if (player.getInventory().getItemInMainHand().getType() == Material.NETHERITE_HOE) {
+                event.setDamage(event.getDamage() + 7.5);
             }
         }
     }
