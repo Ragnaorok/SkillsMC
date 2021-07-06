b2fbc42260cefeb45a4158d82bf8d8cdb0620a4c
Ragnaorok <Tung02nguyen10@gmail.com>
2021-07-06 14:03:23 -0700

    added enchant check command and fake enchantment "reinforce"

--- src/main/java/ragnaorok/Main/Main.java
+++ src/main/java/ragnaorok/Main/Main.java
@@ -5,8 +5,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 import ragnaorok.Main.commands.KaboomCommand;
 import ragnaorok.Main.commands.SaveAllCommand;
 import ragnaorok.Main.commands.SoulsCommand;
+import ragnaorok.Main.customEnchants.ReinforceEnchant;
 import ragnaorok.Main.listeners.*;
 import ragnaorok.Main.listeners.MovementListeners.CrouchJumpListener;
+import ragnaorok.Main.listeners.MovementListeners.DoubleJumpListener;
 import ragnaorok.Main.listeners.PlayerDataListeners.BountyListener;
 import ragnaorok.Main.listeners.PlayerDataListeners.LevelUpListener;
 import ragnaorok.Main.listeners.ToolListeners.*;
@@ -37,6 +39,8 @@ public final class Main extends JavaPlugin {
         this.getCommand("souls").setExecutor(new SoulsCommand(this));
         this.getCommand("kaboom").setExecutor(new KaboomCommand(this));
         this.getCommand("saveAll").setExecutor(new SaveAllCommand(this));
+        this.getCommand("reinforce").setExecutor(new ReinforceEnchant( this));
+        this.getCommand("check").setExecutor(new enchantCheck( this));
         getServer().getPluginManager().registerEvents(new MobListener(), this);
         getServer().getPluginManager().registerEvents(new LevelUpListener(), this);
         getServer().getPluginManager().registerEvents(new CrouchJumpListener(), this);
@@ -44,9 +48,9 @@ public final class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new ShieldListener(), this);
         getServer().getPluginManager().registerEvents(new CrossBowSkill(), this);
         getServer().getPluginManager().registerEvents(new BlazeRodListener(), this);
-        getServer().getPluginManager().registerEvents(new NetheriteHoeListener(), this);
         getServer().getPluginManager().registerEvents(new BountyListener(), this);
-
+        getServer().getPluginManager().registerEvents(new BowListener(), this);
+        getServer().getPluginManager().registerEvents(new NetheriteHoeListener(), this);
         System.out.println("Plugin Enabled");
     }
 
