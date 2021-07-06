e835de777c6e5e1c7c65a287117b8aecdf34490b
Ragnaorok <Tung02nguyen10@gmail.com>
2021-07-06 14:01:50 -0700

    Fixed Hoe interfering with Elytra

--- src/main/java/ragnaorok/Main/listeners/ToolListeners/NetheriteHoeListener.java
+++ src/main/java/ragnaorok/Main/listeners/ToolListeners/NetheriteHoeListener.java
@@ -8,8 +8,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.entity.EntityToggleGlideEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.util.Vector;
 
 import java.util.HashMap;
@@ -30,10 +30,38 @@ public class NetheriteHoeListener implements Listener {
         player.setGliding(false);
     }
 
+    @EventHandler
+    public void onCooldown(PlayerMoveEvent event) { //Calls groundUpdate
+        Player player = event.getPlayer();  //player
+        if (leftCooldown.containsKey(player.getName())) {
+            if (leftCooldown.get(player.getName()) == System.currentTimeMillis()) {
+                player.sendMessage(ChatColor.GREEN + "Phantom Rush is ready");
+            }
+        }
+    }
+
     @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerMove(PlayerMoveEvent event) { //Calls groundUpdate
         if (!event.getPlayer().isGliding()) return;
         this.groundUpdate(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onGlideEvent(EntityToggleGlideEvent event) {
+        Player player = (Player) event.getEntity();
+        if (player.getItemInHand().getType() == Material.NETHERITE_HOE){
+            event.setCancelled(true);
+    }
+}
+
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageByEntityEvent event) { //Cancels glide if player takes damage while gliding
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            if (player.isGliding())
+                player.setGliding(false);
+        }
     }
 
     @EventHandler
@@ -52,11 +80,10 @@ public class NetheriteHoeListener implements Listener {
                     if (leftCooldown.containsKey(player.getName())) {
                         if (leftCooldown.get(player.getName()) > System.currentTimeMillis()) {
                             long time = (leftCooldown.get(player.getName()) - System.currentTimeMillis()) / 1000;
-                            player.sendMessage(ChatColor.DARK_GRAY + "Phantom Rush will be ready in " + time + " second(s)");
                             return;
                         }
                     }
-                    leftCooldown.put(player.getName(), System.currentTimeMillis() + (5 * 1000));
+                    leftCooldown.put(player.getName(), System.currentTimeMillis() + (6 * 1000));
                     player.sendMessage(ChatColor.GREEN + "Scythe Skill: Phantom Rush");
                     direction.normalize();
                     player.setVelocity(direction);
@@ -66,11 +93,11 @@ public class NetheriteHoeListener implements Listener {
                     if (shiftCooldown.containsKey(player.getName())) {
                         if (shiftCooldown.get(player.getName()) > System.currentTimeMillis()) {
                             long time = (shiftCooldown.get(player.getName()) - System.currentTimeMillis()) / 1000;
-                            player.sendMessage(ChatColor.DARK_GRAY + "Cursed Bullet will be ready in " + time + " second(s)");
+                            player.sendMessage(ChatColor.DARK_GRAY + "Shulker Bullet will be ready in " + time + " second(s)");
                             return;
                         }
                     }
-                    shiftCooldown.put(player.getName(), System.currentTimeMillis() + (7 * 1000));
+                    shiftCooldown.put(player.getName(), System.currentTimeMillis() + (8 * 1000));
                     player.sendMessage(ChatColor.GREEN + "Scythe Skill: Shulker Bullet");
                     destination.add(direction).add(direction);
                     Entity entity = world.spawnEntity(new Location(world, destination.getX(), destination.getY() - 1, destination.getZ()), entityType);
@@ -78,15 +105,19 @@ public class NetheriteHoeListener implements Listener {
                     entity.setVelocity(vec.multiply(4));
                 }
             }
+            if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                player.sendMessage(ChatColor.RED + "You cannot use your offhand");
+                event.setCancelled(true);
+            }
         }
     }
 
     @EventHandler
-    public void reinforceEnchant(EntityDamageByEntityEvent event) {
+    public void damageModifier(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player && event.getEntity() instanceof Monster || event.getDamager() instanceof Player && event.getEntity() instanceof Player) {
             Player player = (Player) event.getDamager();
             if (player.getInventory().getItemInMainHand().getType() == Material.NETHERITE_HOE) {
-                event.setDamage(event.getDamage() + 7.5);
+                event.setDamage(event.getDamage() + 8);
             }
         }
     }
