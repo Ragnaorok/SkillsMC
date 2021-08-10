19fdf3424276099f227377663e4a69786a75fad4
Ragnaorok <Tung02nguyen10@gmail.com>
2021-08-09 19:25:43 -0700

    ClassType Enum  & methods

---
+++ src/main/java/ragnaorok/Main/ClassType.java
@@ -0,0 +1,76 @@
+package ragnaorok.Main;
+
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+public enum ClassType {
+    NONE(""),
+    WARRIOR(ChatColor.RED + "WARRIOR"),
+    MAGE(ChatColor.LIGHT_PURPLE + "MAGE"),
+    CLERIC(ChatColor.YELLOW + "CLERIC"),
+    ARCHER(ChatColor.GREEN + "ARCHER");
+
+    private final String prefix;
+
+    ClassType(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public static void setPlayerClassType(OfflinePlayer player, ClassType type) {
+        String playerUUID = player.getUniqueId().toString();
+        Currency.CLASSTYPE.put(playerUUID, type);
+    }
+
+    public static ClassType getPlayerClassType(OfflinePlayer player) {
+        String playerUUID = player.getUniqueId().toString();
+        Currency.CLASSTYPE.putIfAbsent(playerUUID, NONE);
+        return Currency.CLASSTYPE.get(playerUUID);
+    }
+
+    public static void loadPlayerClassTypeFile() throws Exception {
+
+        File file = new File("ClassType.dat");
+        boolean successful = true;
+
+        if (!file.exists()) {
+            successful = file.createNewFile();
+        }
+
+        if (!successful)
+            return;
+
+        try (ObjectInputStream input = new ObjectInputStream(new GZIPInputStream(new FileInputStream(file)))) {
+            HashMap<String, ClassType> readObject = (HashMap<String, ClassType>) input.readObject();
+            for (String key : readObject.keySet()) {
+                ClassType type = readObject.get(key);
+                Currency.CLASSTYPE.put(key, type);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void savePlayerClassTypeFile() throws Exception {
+        File file = new File("ClassType.dat");
+        boolean successful = true;
+        if (!file.exists()) {
+            successful = file.createNewFile();
+        }
+
+        if (!successful)
+            return;
+
+        try (ObjectOutputStream output = new ObjectOutputStream(new GZIPOutputStream(new FileOutputStream(file)))) {
+            output.writeObject(Currency.CLASSTYPE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+}
