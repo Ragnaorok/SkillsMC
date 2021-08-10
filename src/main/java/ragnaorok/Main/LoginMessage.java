83ecf1ac2116842477b94fde68546c6f3b1f649f
Ragnaorok <Tung02nguyen10@gmail.com>
2021-08-09 19:35:25 -0700

    New login ambience and plugin version

--- src/main/java/ragnaorok/Main/LoginMessage.java
+++ src/main/java/ragnaorok/Main/LoginMessage.java
@@ -1,11 +1,25 @@
 package ragnaorok.Main;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 
-public class LoginMessage implements Listener {
+import static org.bukkit.Sound.*;
+
+public class loginMessage implements Listener {
     @EventHandler
-    public void onLogin(){
-
+    public void onLogin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        World world = player.getWorld();
+        Location loc = player.getLocation();
+        Location soundLoc = loc.clone();
+        player.sendMessage("Welcome to RagnarokMC V:1.9 (Alpha)" + ChatColor.ITALIC);
+        world.playSound(soundLoc,BLOCK_BEACON_AMBIENT,73,1);
+        world.playSound(soundLoc,ENTITY_EXPERIENCE_ORB_PICKUP,73,1);
     }
 }
