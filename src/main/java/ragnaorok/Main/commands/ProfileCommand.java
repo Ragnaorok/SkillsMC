5b12e1e5dce9fcecf17ddf99989e0df0117216ea
Ragnaorok <Tung02nguyen10@gmail.com>
2021-08-09 19:34:13 -0700

    New Class displays profile, only player class for now

---
+++ src/main/java/ragnaorok/Main/commands/ProfileCommand.java
@@ -0,0 +1,43 @@
+package ragnaorok.Main.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import ragnaorok.Main.ClassType;
+import ragnaorok.Main.Main;
+
+public class ProfileCommand implements CommandExecutor {
+
+    public Main plugin;
+
+    public ProfileCommand(Main plugin) {
+        this.plugin = plugin;
+        plugin.getCommand("profile").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (label.equalsIgnoreCase("profile")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.RED + "You cannot run this command!");
+                return true;
+            }
+            Player player = (Player) sender;
+            player.sendMessage(ChatColor.GREEN + ClassType.getPlayerClassType(player).toString());
+            return true;
+        }
+        return false;
+    }
+}
+
+
