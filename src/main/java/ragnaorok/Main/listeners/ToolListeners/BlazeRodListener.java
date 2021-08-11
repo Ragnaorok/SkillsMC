b65c382066172bda9b755c0a4af5a77461962b77
Ragnaorok <Tung02nguyen10@gmail.com>
2021-08-10 19:15:42 -0700

    Removed unnecessary imports; reimplemented explosion to fireballs

--- src/main/java/ragnaorok/Main/listeners/ToolListeners/BlazeRodListener.java
+++ src/main/java/ragnaorok/Main/listeners/ToolListeners/BlazeRodListener.java
@@ -1,35 +1,21 @@
 package ragnaorok.Main.listeners.ToolListeners;
 
 import org.bukkit.*;
-import org.bukkit.block.Block;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockExplodeEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
-import org.bukkit.event.entity.ExplosionPrimeEvent;
-import org.bukkit.event.entity.ProjectileHitEvent;
-import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
-import org.bukkit.util.Vector;
-import ragnaorok.Main.ClassType;
+import ragnaorok.Main.managers.ManaManager;
 import ragnaorok.Main.managers.PlayerClassManager;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.bukkit.Sound.*;
 import static org.bukkit.entity.EntityType.FIREBALL;
 
 public class BlazeRodListener implements Listener {
-    Map<String, Long> leftCooldown = new HashMap<String, Long>();
-    Map<String, Long> shiftCooldown = new HashMap<String, Long>();
-
 
     @EventHandler
     public void onCast(PlayerInteractEvent event) {
@@ -39,15 +25,19 @@ public class BlazeRodListener implements Listener {
             PlayerClassManager.displayManaBar(player);
         }
         if (event.getAction() == Action.RIGHT_CLICK_AIR) {
-            PlayerClassManager.addMana();
+            if (ManaManager)
+            ManaManager.addMana(player, 1);
             PlayerClassManager.displayManaBar(player);
         }
-        }
+    }
 
     @EventHandler
     public void onExplode(EntityExplodeEvent event) {
         EntityType type = event.getEntityType();
+        World world = event.getLocation().getWorld();
         if (type != FIREBALL) return;
+        Location loc = event.getLocation();
+        world.createExplosion(loc,1 , false, false);
         event.setCancelled(true);
     }
 }
