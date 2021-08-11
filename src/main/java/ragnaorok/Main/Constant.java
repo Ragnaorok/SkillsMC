4e14422cc170e867863ed4232549cbbf3f37a4d0
Ragnaorok <Tung02nguyen10@gmail.com>
2021-08-10 19:13:41 -0700

    renamed class to Constant

--- src/main/java/ragnaorok/Main/Constant.java
+++ src/main/java/ragnaorok/Main/Constant.java
@@ -3,9 +3,10 @@ package ragnaorok.Main;
 import java.util.HashMap;
 import java.util.UUID;
 
-public class Currency {
+public class Constant {
     public static final HashMap<String, Integer> SOULS = new HashMap<>();
     public static final HashMap<String, Integer> BOUNTIES = new HashMap<>();
+    public static final HashMap<String, Integer> MANA = new HashMap<>();
     public static final HashMap<String, ClassType> CLASSTYPE = new HashMap<>();
 
 }
