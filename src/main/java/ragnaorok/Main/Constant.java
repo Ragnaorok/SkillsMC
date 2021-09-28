900a611a1315b51bac2d965d51110286aceb1f0f
Ragnaorok <Tung02nguyen10@gmail.com>
2021-09-28 00:10:12 -0700

    remove unnecessary imports

--- src/main/java/ragnaorok/Main/Constant.java
+++ src/main/java/ragnaorok/Main/Constant.java
@@ -1,7 +1,6 @@
 package ragnaorok.Main;
 
 import java.util.HashMap;
-import java.util.UUID;
 
 public class Constant {
     public static final HashMap<String, SkillsMCPlayer> SKILLS_MC_PLAYER_HASH_MAP = new HashMap<>();
