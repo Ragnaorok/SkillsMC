3c05f2bf1bd3c391812c84961c9c5533a90a5bee
Ragnaorok <Tung02nguyen10@gmail.com>
2021-09-29 15:40:20 -0700

    add air to NONE enum

--- src/main/java/ragnaorok/Main/Tools.java
+++ src/main/java/ragnaorok/Main/Tools.java
@@ -3,21 +3,22 @@ package ragnaorok.Main;
 import org.bukkit.Material;
 import org.bukkit.inventory.PlayerInventory;
 
-public enum Tools{
+public enum Tools {
     NONE,
     SWORD,
     AXE;
 
-        public static Enum getMainHand(PlayerInventory inventory) {
-            Enum mainHand = inventory.getItemInMainHand().getType();
-            if (mainHand == Material.WOODEN_SWORD || mainHand == Material.STONE_SWORD || mainHand == Material.IRON_SWORD || mainHand == Material.DIAMOND_SWORD|| mainHand == Material.NETHERITE_SWORD){
-                mainHand = SWORD;
-            }
-            if (mainHand == Material.WOODEN_AXE || mainHand == Material.STONE_AXE || mainHand == Material.IRON_AXE || mainHand == Material.DIAMOND_AXE|| mainHand == Material.NETHERITE_AXE){
-                mainHand = AXE;
-            }
-            return mainHand;
+    public static Enum getMainHand(PlayerInventory inventory) {
+        Enum mainHand = inventory.getItemInMainHand().getType();
+        if (mainHand == Material.WOODEN_SWORD || mainHand == Material.STONE_SWORD || mainHand == Material.IRON_SWORD || mainHand == Material.DIAMOND_SWORD || mainHand == Material.NETHERITE_SWORD) {
+            mainHand = SWORD;
+        } else if (mainHand == Material.WOODEN_AXE || mainHand == Material.STONE_AXE || mainHand == Material.IRON_AXE || mainHand == Material.DIAMOND_AXE || mainHand == Material.NETHERITE_AXE) {
+            mainHand = AXE;
+        } else if (mainHand == Material.AIR || mainHand == Material.CAVE_AIR ||mainHand == Material.VOID_AIR) {
+            mainHand = NONE;
         }
+        return mainHand;
+    }
 
-        }
+}
 
