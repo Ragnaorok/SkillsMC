8fc4b610d72217ecad7e38c44c527208af6128b4
Ragnaorok <Tung02nguyen10@gmail.com>
2021-09-29 15:40:06 -0700

    update to Data Class methods

--- src/main/java/ragnaorok/Main/PlayerDeathInventory.java
+++ src/main/java/ragnaorok/Main/PlayerDeathInventory.java
@@ -11,14 +11,12 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import ragnaorok.Main.managers.SoulsManager;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import static ragnaorok.Main.SkillsMCPlayer.getSouls;
 
 public class PlayerDeathInventory implements Listener {
     private Inventory gui;
@@ -49,24 +47,25 @@ public class PlayerDeathInventory implements Listener {
     }
 
     public void openNewGui(Player player) {
+        String uuid = player.getUniqueId().toString();
+        SkillsMCPlayer smPlayer = Constant.SKILLS_MC_PLAYER_HASH_MAP.get(uuid);
         int slots = 0;
-        if (getSouls(player) < 25)
+        if (smPlayer.getSouls() < 25)
             slots = 18;
-        else if (getSouls(player) >= 25 && getSouls(player) < 50)
+        else if (smPlayer.getSouls() >= 25 && smPlayer.getSouls() < 50)
             slots = 27;
-        else if (getSouls(player) >= 50 && getSouls(player) < 75)
+        else if (smPlayer.getSouls() >= 50 && smPlayer.getSouls() < 75)
             slots = 36;
-        else if (getSouls(player) >= 75)
+        else if (smPlayer.getSouls() >= 75)
             slots = 45;
-        gui = Bukkit.createInventory(null,slots, ChatColor.BLACK + "Retrieve Items");
+        gui = Bukkit.createInventory(null, slots, ChatColor.BLACK + "Retrieve Items");
         ItemStack[] content = playerInventory.get(player.getName());
         for (int i = 0; i < gui.getSize(); i++) {
             try {
                 if (content[i] != null) {
                     gui.addItem(content[i]);
                 }
-            }
-            catch(ArrayIndexOutOfBoundsException exception) {
+            } catch (ArrayIndexOutOfBoundsException exception) {
                 continue;
             }
         }
